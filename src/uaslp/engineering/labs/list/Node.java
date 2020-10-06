71451d37712cdc08e2b3dd7dc2a6b78082a3667a
Erika18Granados <erikav021899@gmail.com>
2020-10-05 20:34:46 -0500

    archivos LinkedList copiados

---
+++ src/uaslp/engineering/labs/list/Node.java
@@ -0,0 +1,33 @@
+package uaslp.engineering.labs.list;
+
+import uaslp.engineering.labs.model.Student;
+
+public class Node {
+    public Student student;
+    //SOBRECARGA
+    public Node previous;
+    public Node next;
+
+    public Node(Student student) {
+        this.student = student;
+    }
+    //METODOS
+    public void setStudent(Student student) { //CAMBIAR EL VALOR DE STUDENT DE LA CLASE
+        this.student = student;
+    }
+    public Student getStudent() { //OBTENER STUDENT
+        return student;
+    }
+    public void setPrevious(Node previous) {
+        this.previous = previous;
+    }
+    public Node getPrevious() {
+        return previous;
+    }
+    public void setNext(Node next) {
+        this.next = next;
+    }
+    public Node getNext() {
+        return next;
+    }
+}
